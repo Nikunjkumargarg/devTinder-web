9b44d9f6f4f33019ddb7bb3ec5eb68946f6ee663
NikunjkGarg <nikunjkumar@eupotic.io>
2025-11-02 23:06:51 +0530

    react router and navbar component added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,8 @@
-const navbar = () => {
+const NavBar = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">DevTinder</a>
         </div>
         <div className="flex gap-2">
           <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
@@ -31,4 +31,4 @@ const navbar = () => {
       </div>
     )
 }
-export default navbar;  
+export default NavBar;  
